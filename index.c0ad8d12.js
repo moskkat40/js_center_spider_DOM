5997da5b42e1e74007a3d0eef682098eb2578c1f
Kateryna <moskkat40@gmail.com>
2024-08-20 13:23:29 +0200

    make build

---
+++ index.c0ad8d12.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=(e.clientWidth-t.clientWidth)/2,i=(e.clientHeight-t.clientHeight)/2;t.style.marginLeft="".concat(c,"px"),t.style.marginTop="".concat(i,"px");
+//# sourceMappingURL=index.c0ad8d12.js.map
